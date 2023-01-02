75d908fbbe33ad3f37bfb1cdac8bc49a60933827
Simen <zimen2004@gmail.com>
2023-01-02 15:36:44 +0100

    localstorage test

---
+++ js/script.js
@@ -0,0 +1,19 @@
+if (!localStorage.getItem("score")){
+    localStorage.setItem("score", 0)
+}
+
+let score = parseInt(localStorage.getItem("score"), 10)
+const scoreDisplay = document.querySelector(".score-display")
+scoreDisplay.innerText = score
+
+function itterateScore(i) {
+    score += parseInt(i, 10)
+    scoreDisplay.innerText = score
+    localStorage.setItem("score", score)
+}
+function reset() {
+    score = 0
+    scoreDisplay.innerText = 0
+    localStorage.clear()
+    console.log("hei")
+}
