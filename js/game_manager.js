91c6b7dd2d6403702954afaad1c475e7e1549e83
Simen <zimen2004@gmail.com>
2023-01-04 23:00:07 +0100

    added left right movement

--- js/game_manager.js
+++ js/game_manager.js
@@ -5,7 +5,9 @@ class GameManager{
         this.startingtiles  = startingtiles
 
         // Initialize document refrences
-        this.gridContainer  = document.querySelector(".grid-container")
+        this.highScoreDisplay = document.querySelector(".highscore-display")
+        this.scoreDisplay     = document.querySelector(".score-display")
+        this.gridContainer    = document.querySelector(".grid-container")
 
         //Initialize methods
         this.setupGame()
@@ -13,7 +15,7 @@ class GameManager{
     }
     setupGame(){
         // Check if gameState localstorage key exists,
-        if (localStorage.getItem("gameState") !== null){
+        if ("gameState" in localStorage){
             this.gameState = JSON.parse(localStorage.getItem("gameState"))
         }else{ // If not set to initial gameState
             this.gameState = [
@@ -25,11 +27,12 @@ class GameManager{
             localStorage.setItem("gameState", JSON.stringify(this.gameState))
         }
 
+        //! temp
         this.gameState = [
-            [2048,0,0,2048],
-            [0,0,0,0],
-            [0,0,0,0],
-            [2048,0,0,2048]
+            [2,2,2,2],
+            [2,2,2,2],
+            [2,2,2,2],
+            [2,2,2,2]
         ]
 
         this.updateGridContainer()
@@ -69,12 +72,24 @@ class GameManager{
                 break
             case 1: // Right
                 console.log("right")
+                for (let rowNumber = 0; rowNumber < this.gridSize; rowNumber++) {
+                    let row = this.gameState[rowNumber]
+                    row.reverse()
+                    row = this.slideRow(row)
+                    row.reverse()
+                    this.gameState[rowNumber] = row
+                }
                 break
             case 2: // Down
                 console.log("down")
                 break
             case 3: // Left
                 console.log("left")
+                for (let rowNumber = 0; rowNumber < this.gridSize; rowNumber++) {
+                    let row = this.gameState[rowNumber]
+                    row = this.slideRow(row)
+                    this.gameState[rowNumber] = row
+                }
                 break
             default:
                 break;
@@ -83,11 +98,37 @@ class GameManager{
         this.updateGridContainer()
         this.updateScoreDisplay() //TODO
     }
+    slideRow(row) {
+        row = this.removeZeroes(row)
+        for (let i = 0; i < row.length - 1; i++) {
+            if (row[i] == row[i+1]){
+                row[i]  *= 2
+                row[i+1] = 0
+                // score += row
+            }
+        }
+        row = this.removeZeroes(row)
+        while (row.length < this.gridSize){
+            row.push(0)
+        }
+        return row
+    }
+    removeZeroes(row) {
+        return row.filter(num => num != 0)
+    }
+    //? Unnecessary
+    // getRow(rowNumber){
+    //     let row = []
+    //     for (let columnNumber = 0; columnNumber < this.gridSize; columnNumber++) {
+    //         row.push(this.gameState[rowNumber][columnNumber])
+    //     }
+    //     return row
+    // }
+
     updateGridContainer(){
         this.gridContainer.innerHTML = ""
-
         for (let row = 0; row < this.gridSize; row++) {
-        for (let column = 0; column < this.gridSize; column++) {
+            for (let column = 0; column < this.gridSize; column++) {
                 let tile = document.createElement("div")
                 tile.id = row.toString() + "-" + column.toString() 
                 tile.classList.add("tile")
@@ -96,8 +137,11 @@ class GameManager{
                     tile.classList.add("tile-"+ tileNumber.toString())
                     tile.innerHTML = tileNumber
                 }
-                this.gridContainer.appendChild(tile)
+            this.gridContainer.appendChild(tile)
         }}
+    }
+    updateScoreDisplay(){
+        //TODO: Add scores in general
     }
     restart(){
         //TODO: Reset points, localstorage, grid, 
